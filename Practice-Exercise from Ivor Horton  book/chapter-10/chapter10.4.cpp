75f9b47d9914fff4ef77d660dcd1fe256e942ee4
Abhay sharma <68181980+abhaysharmak90@users.noreply.github.com>
2022-08-04 17:47:59 +0000

    10.4_started

---
+++ Practice-Exercise from Ivor Horton  book/chapter-10/chapter10.4.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<span>
+#include<vector>
+#include<array>
+
+template <typename t>
+auto arr_size(t vec);
+
+int main()
+{
+    std::vector<int> v1{222,775,554,668,4432,77985,5,4,3,8,9};
+    int a1[]{886,774,666,55,779,4,7,5,3,8};
+    std::array<int,20> a2{556,446,553,24,222,6,7,6,3,2,779,6,4,2,4,6,9};
+
+    std::cout<<arr_size(v1);
+    std::cout<<arr_size(a1);
+    std::cout<<arr_size(a2);
+}
