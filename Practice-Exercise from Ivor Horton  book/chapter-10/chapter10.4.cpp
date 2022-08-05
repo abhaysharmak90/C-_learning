83654c3364786309f0e9c1aee75f759984ac2a23
Abhay sharma <68181980+abhaysharmak90@users.noreply.github.com>
2022-08-05 12:01:16 +0000

    __

--- Practice-Exercise from Ivor Horton  book/chapter-10/chapter10.4.cpp
+++ Practice-Exercise from Ivor Horton  book/chapter-10/chapter10.4.cpp
@@ -1,14 +1,14 @@
-#include<iostream>
-#include<vector>
-#include<array>
+#include <iostream>
+#include <vector>
+#include <array>
 
-//template <typename t>
-//auto arr_size(t vec);
+// template <typename t>
+// auto arr_size(t vec);
 template <typename t>
 auto arr_size(t vec)
 {
-    size_t size{},indx{0};
-    while(vec[indx])
+    size_t size{}, indx{0};
+    while (vec[indx])
     {
         size++;
         indx++;
@@ -18,11 +18,10 @@ auto arr_size(t vec)
 
 int main()
 {
-    std::vector<int> v1{222,775,554,668,4432,77985,5,4,3,8,9};
-    int a1[]{886,774,666,55,779,4,7,5,3,8};
-    std::array<int,20> a2{556,446,553,24,222,6,7,6,3,2,779,6,4,2,4,6,9};
-    std::cout<<arr_size(v1);
-    std::cout<<arr_size(a1);
-    std::cout<<arr_size(a2);
+    std::vector<int> v1{222, 775, 554, 668, 4432, 77985, 5, 4, 3, 8, 9};
+    int a1[]{886, 774, 666, 55, 779, 4, 7, 5, 3, 8};
+    std::array<int, 20> a2{556, 446, 553, 24, 222, 6, 7, 6, 3, 2, 779, 6, 4, 2, 4, 6, 9};
+    std::cout << arr_size(v1) << "\n";
+    std::cout << arr_size(a1) << "\n";
+    std::cout << arr_size(a2) << "\n";
 }
-
