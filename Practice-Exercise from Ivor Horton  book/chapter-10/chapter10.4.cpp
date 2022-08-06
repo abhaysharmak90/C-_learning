741b93355b5a5b6cc4344f6cf0286bb0ef540a53
Abhay sharma <68181980+abhaysharmak90@users.noreply.github.com>
2022-08-06 08:58:17 +0000

    10.4_Complete

--- Practice-Exercise from Ivor Horton  book/chapter-10/chapter10.4.cpp
+++ Practice-Exercise from Ivor Horton  book/chapter-10/chapter10.4.cpp
@@ -3,17 +3,35 @@
 #include <array>
 
 // template <typename t>
-// auto arr_size(t vec);
+// auto arr_size(t vec);                    cannot use auto if defination is after main()
 template <typename t>
-auto arr_size(t vec)
+size_t arr_size(const t &vec)
 {
-    size_t size{}, indx{0};
-    while (vec[indx])
-    {
-        size++;
-        indx++;
-    }
-    return size;
+    // size_t size{0}, indx{0};
+    // while (vec[indx])
+    // {
+    //     size++;
+    //     indx++;
+    // }
+    // return size;
+    std::cout<<"vec,arr<>called\n";
+    return vec.size();                          //saw answer 
+}
+
+template <typename t,size_t n>
+size_t arr_size(const t (&vec)[n])
+{
+    // size_t size{0}, indx{0};
+    // while (vec[indx])                        didn't work kept going ut of bounds
+    // {
+    //     size++;
+    //     std::cout<<size<<" "<<indx<<" "<<vec[indx]<<std::endl;
+    //     indx++;
+    // }
+
+    std::cout<<"arr_called\n";
+    // return size;
+    return n;                           //saw answer
 }
 
 int main()
@@ -22,6 +40,7 @@ int main()
     int a1[]{886, 774, 666, 55, 779, 4, 7, 5, 3, 8};
     std::array<int, 20> a2{556, 446, 553, 24, 222, 6, 7, 6, 3, 2, 779, 6, 4, 2, 4, 6, 9};
     std::cout << arr_size(v1) << "\n";
-    std::cout << arr_size(a1) << "\n";
     std::cout << arr_size(a2) << "\n";
+    std::cout << arr_size(a1) << "\n";
+    std::cout<<std::endl;
 }
