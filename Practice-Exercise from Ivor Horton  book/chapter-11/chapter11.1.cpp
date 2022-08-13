bdf5f1e856872fd944cf2465bbd37b8af970c466
Abhay sharma <68181980+abhaysharmak90@users.noreply.github.com>
2022-08-13 09:52:43 +0000

    enter&show_completed

--- Practice-Exercise from Ivor Horton  book/chapter-11/chapter11.1.cpp
+++ Practice-Exercise from Ivor Horton  book/chapter-11/chapter11.1.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 #include <string>
+#include <memory>
 #include "chapter11_1header.h"
 
 int main()
 {
+    std::shared_ptr<std::string> full_string{std::make_shared<std::string>("")};
+    words::enter_string(full_string);
+    words::show_string(full_string);
 }
--- Practice-Exercise from Ivor Horton  book/chapter-11/chapter11_1header.h
+++ Practice-Exercise from Ivor Horton  book/chapter-11/chapter11_1header.h
@@ -1,8 +1,12 @@
+#include <memory>
 #ifndef chapter11_1
 #define chapter11_1
 
 namespace words
 {
+    void enter_string(std::shared_ptr<std::string>);
+
+    void show_string(std::shared_ptr<std::string>);
 
 }
 #endif
