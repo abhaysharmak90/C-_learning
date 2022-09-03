04cdea0d6255f86327c882ae90ae4d969ca41d59
Abhay sharma <68181980+abhaysharmak90@users.noreply.github.com>
2022-09-03 12:58:18 +0000

    create_vector_function_C

--- Practice-Exercise from Ivor Horton  book/chapter-11/1/chapter11_1header.cpp
+++ Practice-Exercise from Ivor Horton  book/chapter-11/1/chapter11_1header.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include<vectors>
+#include <vector>
 
 #include "chapter11_1header.h"
 
@@ -18,5 +18,37 @@ void words::show_string(std::shared_ptr<std::string> str2)
 
 void words::create_vector(std::shared_ptr<std::string> str3)
 {
-    const std::vector<std::string> seprators{};
+    const std::string SEPRATORS{" ,./<>?;'\n\\[]{}-=_+!@#$%^&*()\t"};
+
+    std::string word{""};
+    std::vector<std::string> words;
+    size_t start_index{}, end_index{}, word_length{};
+
+    start_index = (*str3).find_first_not_of(SEPRATORS);
+    while (start_index != std::string::npos)
+    {
+        // std::cout << "\n"
+        //           << start_index << " " << end_index << " " << word_length << "\n";
+        end_index = (*str3).find_first_of(SEPRATORS, start_index);
+        if (end_index == std::string::npos)
+        {
+            end_index = (*str3).length();
+        }
+        // std::cout << "\n"
+        //           << start_index << " " << end_index << " " << word_length << "\n";
+        word_length = end_index - start_index;
+        word = (*str3).substr(start_index, word_length);
+        // std::cout << std::endl
+        //           << word << std::endl;
+        words.push_back(word);
+        start_index = (*str3).find_first_not_of(SEPRATORS, end_index);
+        // std::cout << "\n"
+        //           << start_index << " " << end_index << " " << word_length << "\n";
+        // exit(0);
+    }
+    for (auto ch : words)
+    {
+        std::cout << "\n"
+                  << ch << "\n";
+    }
 }
