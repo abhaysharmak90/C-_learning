5904acc86bcc60986e15d806496f889b3860ae32
Abhay sharma <68181980+abhaysharmak90@users.noreply.github.com>
2022-09-07 06:39:39 +0000

    12.2_C

--- Practice-Exercise from Ivor Horton  book/chapter-12/2/12.2.cpp
+++ Practice-Exercise from Ivor Horton  book/chapter-12/2/12.2.cpp
@@ -5,9 +5,23 @@ class INTEGER
     int number;
 
 public:
-    void getter() { std::cout << number << "\n"; }
+    void getter() const { std::cout << number << "\n"; }
     void setter(int num) { number = num; }
+    void compare(INTEGER) const;
+    void add(INTEGER num) const;
+    void sub(INTEGER num) const;
+    void multiply(INTEGER num) const;
+
     INTEGER() = default; // 2 ways are either don't put a constructer or add a default keyword at the end
+    INTEGER(auto num) : number{num}
+    {
+        std::cout << "constructor 1 called\n";
+    }
+    INTEGER(const INTEGER &num) : number{num.number}
+    {
+        // std::cout << sizeof(num) << "\n";
+        std::cout << "copy constructor called\n";
+    }
 };
 
 int main()
@@ -22,4 +36,40 @@ int main()
     std::cin >> temp_num;
     numb3.setter(temp_num);
     numb3.getter();
+    numb1.compare(numb3);
+    numb1.add(numb3);
+    numb1.sub(numb3);
+    numb1.multiply(numb3);
+    numb1.add(5);
+    numb1.sub(5);
+    numb1.multiply(5);
+}
+
+void INTEGER::compare(INTEGER t_number) const
+{
+    if (number < t_number.number)
+    {
+        std::cout << "-1\n";
+    }
+    else if (number == t_number.number)
+    {
+        std::cout << "0\n";
+    }
+    else
+    {
+        std::cout << "1\n";
+    }
+}
+
+void INTEGER::add(INTEGER num) const
+{
+    std::cout << "sum is" << number + num.number << std::endl;
+}
+void INTEGER::sub(INTEGER num) const
+{
+    std::cout << "sub is" << number - num.number << std::endl;
+}
+void INTEGER::multiply(INTEGER num) const
+{
+    std::cout << "multi is" << number * num.number << std::endl;
 }
