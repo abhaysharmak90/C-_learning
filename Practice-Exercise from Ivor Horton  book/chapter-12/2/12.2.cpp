cfe43d68623c594dc813ced32d04dcb6dadca871
Abhay sharma <68181980+abhaysharmak90@users.noreply.github.com>
2022-09-09 08:52:13 +0000

    --

--- Practice-Exercise from Ivor Horton  book/chapter-12/2/12.2.cpp
+++ Practice-Exercise from Ivor Horton  book/chapter-12/2/12.2.cpp
@@ -8,15 +8,16 @@ public:
     void getter() const { std::cout << number << "\n"; }
     void setter(int num) { number = num; }
     void compare(INTEGER) const;
-    void add(INTEGER num) const;
-    void sub(INTEGER num) const;
-    void multiply(INTEGER num) const;
+    // void compare(INTEGER &) const;
+    // void add(INTEGER num) const;
+    // void sub(INTEGER num) const;
+    // void multiply(INTEGER num) const;
 
     INTEGER() = default; // 2 ways are either don't put a constructer or add a default keyword at the end
-    INTEGER(auto num) : number{num}
-    {
-        std::cout << "constructor 1 called\n";
-    }
+    // INTEGER(auto num) : number{num}
+    // {
+    //     std::cout << "constructor 1 called\n";
+    // }
     INTEGER(const INTEGER &num) : number{num.number}
     {
         // std::cout << sizeof(num) << "\n";
@@ -37,16 +38,33 @@ int main()
     numb3.setter(temp_num);
     numb3.getter();
     numb1.compare(numb3);
-    numb1.add(numb3);
-    numb1.sub(numb3);
-    numb1.multiply(numb3);
-    numb1.add(5);
-    numb1.sub(5);
-    numb1.multiply(5);
+    // numb1.add(numb3);
+    // numb1.sub(numb3);
+    // numb1.multiply(numb3);
+    // numb1.add(5);
+    // numb1.sub(5);
+    // numb1.multiply(5);
 }
 
+// void INTEGER::compare(INTEGER &t_number) const
+// {
+//     std::cout << "this is by reference\n";
+//     if (number < t_number.number)
+//     {
+//         std::cout << "-1\n";
+//     }
+//     else if (number == t_number.number)
+//     {
+//         std::cout << "0\n";
+//     }
+//     else
+//     {
+//         std::cout << "1\n";
+//     }
+// }
 void INTEGER::compare(INTEGER t_number) const
 {
+    std::cout << "this is by reference\n";
     if (number < t_number.number)
     {
         std::cout << "-1\n";
@@ -61,15 +79,7 @@ void INTEGER::compare(INTEGER t_number) const
     }
 }
 
-void INTEGER::add(INTEGER num) const
-{
-    std::cout << "sum is" << number + num.number << std::endl;
-}
-void INTEGER::sub(INTEGER num) const
-{
-    std::cout << "sub is" << number - num.number << std::endl;
-}
-void INTEGER::multiply(INTEGER num) const
-{
-    std::cout << "multi is" << number * num.number << std::endl;
-}
+
+
+// so when we use pass by reference copy constructor is not called
+//but when pass by value is used copy constructor is used so it is always good to use pass by reference is possible
--- Practice-Exercise from Ivor Horton  book/chapter-12/3/12.3.cpp
+++ Practice-Exercise from Ivor Horton  book/chapter-12/3/12.3.cpp
@@ -1,9 +1,66 @@
 #include <iostream>
 
-class
+void calculate(INTEGER &, INTEGER &, INTEGER &, INTEGER &, INTEGER &, INTEGER &);
+
+class INTEGER
 {
+    int number;
+
+public:
+    void getter() const { std::cout << number << "\n"; }
+    void setter(int num) { number = num; }
+    // void compare(INTEGER) const;
+    void add(INTEGER &num) const;
+    void sub(INTEGER &num) const;
+    void multiply(INTEGER &num) const;
+
+    INTEGER() = default; // 2 ways are either don't put a constructer or add a default keyword at the end
+    INTEGER(int num) : number{num}
+    {
+        std::cout << "constructor 1 called\n";
+    }
+    // INTEGER(const INTEGER &num) : number{num.number}
+    // {
+    //     // std::cout << sizeof(num) << "\n";
+    //     std::cout << "copy constructor called\n";
+    // }
 };
 
 int main()
+{
+    INTEGER numb4{4}, numb5{5}, numb6{6}, numb7{7}, numb8{8};
+    std::cout << "answer to equation is:\n";
+    // int temp_num;
+    // std::cout << "enter a number:\n";
+    // std::cin >> temp_num;
+    // numb1.setter(temp_num);
+    // numb1.getter();
+    // std::cout << "enter a number:\n";
+    // std::cin >> temp_num;
+    // numb3.setter(temp_num);
+    // numb3.getter();
+    // // numb1.compare(numb3);
+    // numb1.add(numb3);
+    // numb1.sub(numb3);
+    // numb1.multiply(numb3);
+    // numb1.add(5);
+    // numb1.sub(5);
+    // numb1.multiply(5);
+}
+
+void INTEGER::add(INTEGER &num) const
+{
+    number + num.number;
+}
+void INTEGER::sub(INTEGER &num) const
+{
+    number - num.number;
+}
+void INTEGER::multiply(INTEGER &num) const
+{
+    number *num.number;
+}
+
+void calculate(INTEGER &num4, INTEGER &num5, INTEGER &num6, INTEGER &num7, INTEGER &num8)
 {
 }
